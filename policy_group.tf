4102061e00578a1a172e7441070b1fc987448ae5
aleatoricmbnt <aleatoricmbnt@gmail.com>
2024-01-15 20:10:50 +0200

    fixed resource reference for the policy group

--- policy_group.tf
+++ policy_group.tf
@@ -11,5 +11,5 @@ resource "scalr_policy_group" "policy" {
 
 resource "scalr_policy_group_linkage" "policy_link" {
   policy_group_id = scalr_policy_group.policy.id
-  environment_id  = scalr_environment.env.id
+  environment_id  = scalr_environment.env_no_tag.id
 }
