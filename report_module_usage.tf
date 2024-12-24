4f391753ca2c2932f6d83939675d43cd2c4cb36c
Vladyslav Vlasenko <fl4shqq@gmail.com>
2024-12-24 14:31:44 +0200

    Update report_module_usage.tf

--- report_module_usage.tf
+++ report_module_usage.tf
@@ -9,17 +9,6 @@ resource "scalr_environment" "report_env" {
   cost_estimation_enabled         = false
 }
 
-resource "scalr_module" "report_module" {
-  count = 21
-  environment_id  = scalr_environment.report_env[count.index].id
-  vcs_provider_id = scalr_vcs_provider.vcs.id
-  vcs_repo {
-    identifier = var.module_vcs-repo_identifier
-    path       = var.module_vcs-repo_path
-    tag_prefix = var.module_vcs-repo_tag-prefix
-  }
-}
-
 resource "null_resource" "install_requests" {
   triggers = {
     time = timestamp()
@@ -36,13 +25,6 @@ resource "null_resource" "wait_for_module" {
   provisioner "local-exec" {
     command = "sed 's#url = \"your_url_here\"#url = \"${var.scalr_url}/api/iacp/v3/modules/${scalr_module.report_module[count.index].id}\"#; s#bearer_token = \"your_bearer_token_here\"#bearer_token = \"${var.token}\"#' wait_for_module.py > wait_for_module_${count.index}.py && python3 wait_for_module_${count.index}.py"
   }
-}
-
-data "scalr_module_version" "report_get_modver_id" {
-  count   = 21
-  source  = "${scalr_environment.report_env[count.index].id}/${var.module_name}/${var.module_provider}"
-  version = var.module_existing_version_number
-  depends_on = [ null_resource.wait_for_module]
 }
 
 resource "scalr_workspace" "report_ws" {
