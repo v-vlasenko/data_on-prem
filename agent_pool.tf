c6dc423cbf77127ddaec6e436d2f6ca454246507
Vladyslav Vlasenko <fl4shqq@gmail.com>
2026-02-26 16:37:28 +0200

    Update agent_pool.tf

--- agent_pool.tf
+++ agent_pool.tf
@@ -1,8 +1,9 @@
 resource "scalr_agent_pool" "runs_agent_pool" {
-  name = "agent_pool_${formatdate("DDMMYYYY", timestamp())}"
+  name = "agent_pool_aaa${formatdate("DDMMYYYY", timestamp())}"
 }
 
 resource "scalr_agent_pool_token" "ap-token" {
   description   = "Automatically created/updated at: ${timestamp()}"
   agent_pool_id = scalr_agent_pool.runs_agent_pool.id
 }
+
